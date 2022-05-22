e3d7613a18ee5e4a359e30a68e5f6459bb33f2dd
Panndora1 <valentina_pavlovna1881@mail.ru>
2022-05-22 13:35:41 +0400

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const path = require('path')
+const fs = require('fs')
+
+const pathFileIn = path.join(__dirname, 'files')
+const pathFileOut = path.join(__dirname, 'files-copy')
+
+fs.mkdir(pathFileOut, {recursive: true}, (err) => {
+    if(err) {
+        console.log(err)
+    }
+
+    console.log('Папка создана')
+
+    fs.readdir(pathFileIn, {withFileTypes: true}, (err, files) => {
+        if(err) {
+            console.log(err)
+        }
+    
+        files.forEach(file => {
+            let pathIn = path.join(pathFileIn, file.name)
+            let pathOut = path.join(pathFileOut, file.name)
+
+
+            fs.copyFile(pathIn, pathOut, err => {
+                if(err) {
+                    console.log(err)
+                }
+            })            
+        })
+
+        console.log('Файлы скопированы')
+    });
+})
+
 
